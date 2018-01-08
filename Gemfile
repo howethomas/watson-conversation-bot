5eea5569cdb067f4c6dea1d26015f3da56e1d593
Thomas Howe <ghostofbasho@gmail.com>
2018-01-08 10:16:27 -0500

    Hand coded web calls to avoid Ruby threading error

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'awesome_print'
-gem 'watson-conversation'
 gem 'rest-client'
 gem 'redis'
+gem 'json'
--- bot.rb
+++ bot.rb
@@ -1,24 +1,63 @@
+# This is a bot designed to work with GreenBot https://github.com/green-bot/greenbot-core
+# Simply npm install this bot in the greenbot directory to install.
+#
 require "awesome_print"
-require 'watson/conversation'
+require "rest-client"
+require "json"
 
+# Extract parameters from the Bash environment
+# Greenbot will set these before the bot is executed
 watson_username = ENV['WATSON_USERNAME'] || ''
 password = ENV['PASSWORD'] || ''
 workspace_id = ENV['WORKSPACE_ID'] || ''
-username = ENV['SRC']
-initial_msg = ENV['INITIAL_MSG']
+username = ENV['SRC'] || 'user1'
+initial_msg = ENV['INITIAL_MSG'] || 'hello'
 
-manage = Watson::Conversation::ManageDialog.new(
-  username: watson_username,
-  password: password,
-  workspace_id: workspace_id,
-  storage: 'hash'  
-)
+begin
+  # Start the conversation service.
+  url = "https://#{watson_username}:#{password}@gateway.watsonplatform.net/conversation/api"
+  version="2017-02-03"
+  endpoint = "#{url}/v1/workspaces/#{workspace_id}/message?version=#{version}"
 
-input_text = initial_msg.strip
+  # The first message should be sent to the conversation service.
+  input_text = initial_msg.strip
 
-while input_text != "botquit"
-   response = manage.talk(username, input_text)
-   output_text  = JSON.parse response
-   puts output_text["output"][0]
-   input_text = gets().strip
+  body = {
+    input: {
+      text: input_text
+    }
+  }.to_json
+
+  # Call the endpoint
+  response = RestClient.post endpoint, body, content_type: :json, accept: :json
+
+  # Process the output
+  response_body = JSON.parse(response.body)
+  context = response_body["context"]
+
+  response_body["output"]["text"].each do |text|
+    puts text
+  end
+
+  # While watson doesn't return "botquit", keep forwarding the stdin
+  # to it, express what is says back to stdout.
+
+  unless context["system"]["branch_exited"]
+
+    input_text = gets().strip
+    body = {
+      input: {
+        text: input_text
+      },
+      alternate_intents: true,
+      context: context,
+    }.to_json
+
+    response = RestClient.post endpoint, body, content_type: :json, accept: :json
+    response_body = JSON.parse(response.body)
+    context = response_body["context"]
+    response_body["output"]["text"].each do |text|
+      puts text
+    end
+  end
 end
