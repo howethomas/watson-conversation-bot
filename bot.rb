c14fd2ebbc09a54a3ec1a50d033fca4bedeb5150
Gordon DuQuesnay <gduquesnay@gmail.com>
2018-01-30 20:07:47 +0000

    Updated for tagging and whispers

--- bot.rb
+++ bot.rb
@@ -12,7 +12,7 @@ watson_username = ENV['WATSON_USERNAME'] || ''
 password = ENV['PASSWORD'] || ''
 workspace_id = ENV['WORKSPACE_ID'] || ''
 username = ENV['SRC'] || 'user1'
-initial_msg = ENV['INITIAL_MSG'] || 'hello'
+initial_msg = ENV['INITIAL_MSG'] || 'I need to talk to someone about my bill\n'
 
 begin
   # Start the conversation service.
@@ -20,28 +20,44 @@ begin
   version="2017-02-03"
   endpoint = "#{url}/v1/workspaces/#{workspace_id}/message?version=#{version}"
 
-  puts greeting if greeting?
-  # The first message should be sent to the conversation service.
-  input_text = gets.strip
-
   body = {
     input: {
-      text: input_text
+      text: initial_msg.strip
     }
   }.to_json
 
   # Call the endpoint
   response = RestClient.post endpoint, body, content_type: :json, accept: :json
-
+  
   # Process the output
   response_body = JSON.parse(response.body)
   context = response_body["context"]
 
   response_body["output"]["text"].each do |text|
-    puts text
+    puts text+"\n"
+    $stdout.flush
   end
 
-  # While watson doesn't return "botquit", keep forwarding the stdin
+  # Whisper to the agent the intent of the customer
+  response_body["intents"].each do |i|
+    confidenceScore = i['confidence'].to_f
+    puts "{{whisper: customer intent: #{i['intent']}: #{confidenceScore.round(2)*100} % confidence}}"
+    $stdout.flush
+  
+    # If the confidence is greater than 50%, tag the customer with the intent
+    if confidenceScore > 0.5
+      puts "{{tag: #{i['intent']}}}"
+      $stdout.flush
+    end
+  end
+  
+  response_body["intents"].each do |i|
+    puts "{{whisper: customer intent: #{i['intent']}: #{i['confidence'].to_f.round(2)*100} % confidence}}"
+    $stdout.flush
+  end
+
+
+  # While watson doesn't return the string "botquit", keep forwarding the stdin
   # to it, express what is says back to stdout.
 
   unless context["system"]["branch_exited"]
@@ -49,7 +65,7 @@ begin
     input_text = gets().strip
     body = {
       input: {
-        text: input_text
+        text: input_text.strip
       },
       alternate_intents: true,
       context: context,
@@ -59,7 +75,10 @@ begin
     response_body = JSON.parse(response.body)
     context = response_body["context"]
     response_body["output"]["text"].each do |text|
-      puts text
+      puts text+"\n"
+      $stdout.flush
     end
   end
+  puts "{{whisper: Automation exited (#{context['system']['branch_exited_reason']}) }}" 
+  $stdout.flush
 end
